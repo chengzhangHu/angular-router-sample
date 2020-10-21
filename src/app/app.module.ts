3610c30ff9647d9bc04962b071b8be06f0f22427
chengzhanghu <simple2012hcz@126.com>
2020-10-21 16:49:37 +0800

    feat(demo): add the hero demos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { CrisisListComponent } from './crisis-list/crisis-list.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeroesModule } from './heroes/heroes.module';
+
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HeroesModule,
     AppRoutingModule
@@ -19,7 +21,7 @@ import { HeroesModule } from './heroes/heroes.module';
   declarations: [
     AppComponent,
     CrisisListComponent,
-    PageNotFoundComponent,
+    PageNotFoundComponent
   ],
   bootstrap: [ AppComponent ]
 })
